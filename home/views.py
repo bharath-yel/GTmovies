bdc7ef831cc103ac879d305f5b12f432797e6768
imermigkas3 <mermigasj@gmail.com>
2025-01-28 15:58:29 -0500

    fixed small bug

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 def index(request):
+    template_data = {}
+    template_data['title'] = 'GTmovies'
     #render is used to render templates and return an HTTP response
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', {
+        'template_data' : template_data})
 
 def about(request):
     return render(request, 'home/about.html')
