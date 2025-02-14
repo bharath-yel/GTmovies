a3487cb668e434994f31ca776afde9405b8f1b1d
Yashil Patel <yashilsmacbook@Yashils-MacBook-Pro.local>
2025-02-13 22:41:11 -0500

    Added email to sign up

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.forms.utils import ErrorList
 from django.utils.safestring import mark_safe
 from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+from django import forms
 
 class CustomErrorList(ErrorList):
     def __str__(self):
@@ -13,15 +15,21 @@ class CustomErrorList(ErrorList):
             f'<div class="alert alert-danger" role="alert">{e}</div>' for e in self
         ]))
 
+User = get_user_model()
+
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control'}),
+    )
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        for fieldname in ['username', 'password1', 'password2']:
+        for fieldname in self.fields:
             self.fields[fieldname].help_text = None
             self.fields[fieldname].widget.attrs.update({'class': 'form-control'})
-
-
-User = get_user_model()
 
 class CustomPasswordResetForm(PasswordResetForm):
     def clean_email(self):
