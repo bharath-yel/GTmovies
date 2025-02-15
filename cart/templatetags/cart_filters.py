67aeeeea02c64807dd75bbd3eef0fc79e955c982
Akhil Gopal <agopal45@gatech.edu>
2025-02-15 11:38:57 -0500

    added subtotal

--- cart/templatetags/cart_filters.py
+++ cart/templatetags/cart_filters.py
@@ -2,4 +2,8 @@ from django import template
 register = template.Library()
 @register.filter(name='get_quantity')
 def get_cart_quantity(cart, movie_id):
-    return cart[str(movie_id)]
+    return int(cart[str(movie_id)])
+
+@register.filter
+def multiply(x, y):
+    return x * y
